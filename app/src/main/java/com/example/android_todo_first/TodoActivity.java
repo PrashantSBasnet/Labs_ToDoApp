170885db3f8731a67d7ea3ce7e45c6388df103de
Ebbi <ebbi40@gmail.com>
2019-11-27 21:33:43 +0000

    comments added and tidy up

--- app/src/main/java/com/example/android_todo_first/TodoActivity.java
+++ app/src/main/java/com/example/android_todo_first/TodoActivity.java
@@ -15,6 +15,7 @@ public class TodoActivity extends AppCompatActivity {
     /* In case of state change, such as rotating the phone,
        store the mTodoIndex */
     private static final String TODO_INDEX = "com.example.android_todo_first.todoIndex";
+
     /* override to write the value of mTodoIndex into
        the Bundle with TODO_INDEX as its key */
     @Override
@@ -26,40 +27,48 @@ public class TodoActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-    /* check for saved state due to changes such as rotation
-        and restore any saved state such as the TODO_INDEX */
+        /* check for saved state due to changes such as rotation
+            and restore any saved state such as the TODO_INDEX */
         if (savedInstanceState != null){
             mTodoIndex = savedInstanceState.getInt(TODO_INDEX, 0);
         }
 
-      /* call the super class onCreate to complete the creation
-         of activity with state changes */
+        /* call the super class onCreate to complete the creation
+            of activity with  any state changes */
         super.onCreate(savedInstanceState);
 
-        // set the user interface layout for this Activity
-        setContentView(R.layout.activity_todo);
-
-        // initialize member TextView so we can manipulate it later
-        final TextView TodoTextView;
-        TodoTextView = (TextView) findViewById(R.id.textViewTodo);
-
-        // read the todo array from res/values/strings.xml
+        /*  Refactor model
+            read into mTodos array from res/values/strings.xml */
         Resources res = getResources();
         mTodos = res.getStringArray(R.array.todos);
 
-        // display the first task from mTodo array in the TodoTextView
+        /* set the user interface layout for this Activity */
+        setContentView(R.layout.activity_todo);
+
+        /* initialize member TextView so we can manipulate it later */
+        final TextView TodoTextView;
+        TodoTextView = (TextView) findViewById(R.id.textViewTodo);
+
+        /* display the first task from mTodo array in the TodoTextView */
         TodoTextView.setText(mTodos[mTodoIndex]);
 
+        /* setup navigation buttons */
+        /* next button to cycle through mTodos */
         Button buttonNext;
         buttonNext = (Button) findViewById(R.id.buttonNext);
 
-        // OnClick listener for the  Next button
-        buttonNext.setOnClickListener(new View.OnClickListener(){
+        /* OnClick listener for the  Next button */
+        buttonNext.setOnClickListener(
+                new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 mTodoIndex += 1;
                 TodoTextView.setText(mTodos[mTodoIndex]);
             }
         });
+
+        /** TODO setup previous button object and listener
+        *   to cycle through mTodos */
+
     }
 }
