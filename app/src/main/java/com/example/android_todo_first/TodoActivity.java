2a6db5b3b9826a08b33243eab2c10f72dd582880
Prashant <prasbas97@gmail.com>
2021-03-17 11:26:07 +0545

    Added ToDo Details app after fixing null pointer error

--- app/src/main/java/com/example/android_todo_first/TodoActivity.java
+++ app/src/main/java/com/example/android_todo_first/TodoActivity.java
@@ -1,108 +1,171 @@
 package com.example.android_todo_first;
 
+import android.content.Intent;
 import android.content.res.Resources;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.android_todo_first.R;
+import com.example.android_todo_first.TodoDetailActivity;
 
 public class TodoActivity extends AppCompatActivity {
 
+    private static final int IS_SUCCESS = 0;
     private String[] mTodos;
     private int mTodoIndex = 0;
-    private TextView TodoTextView;
-    /* In case of state change, such as rotating the phone,
-       store the mTodoIndex */
-    private static final String TODO_INDEX = "com.example.android_todo_first.todoIndex";
+
+    public static final String TAG = "TodoActivity";
+
+    /* map or name, value pair to be returned in an intent */
+    private static final String IS_TODO_COMPLETE = "com.example.isTodoComplete";
+
+    /** In case of state change, due to rotating the phone
+     * store the mTodoIndex to display the same mTodos element post state change
+     * N.B. small amounts of data, typically IDs can be stored as key, value pairs in a Bundle
+     * the alternative is to abstract view data to a ViewModel which can be in scope in all
+     * Activity states and more suitable for larger amounts of data */
+
+    private static final String TODO_INDEX = "com.example.todoIndex";
+
+    /* override to write the value of mTodoIndex into the Bundle with TODO_INDEX as its key */
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        savedInstanceState.putInt(TODO_INDEX, mTodoIndex);
+        super.onSaveInstanceState(savedInstanceState);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        /* check for saved state due to changes such as rotation
-            and restore any saved state such as the TODO_INDEX */
         if (savedInstanceState != null){
             mTodoIndex = savedInstanceState.getInt(TODO_INDEX, 0);
         }
-
-        /* call the super class onCreate to complete the creation
-            of activity with  any state changes */
+        /* call the super class onCreate to complete the creation of Activity
+           like the view hierarchy */
         super.onCreate(savedInstanceState);
 
-        /*  Refactor model
-            read into mTodos array from res/values/strings.xml */
-        Resources res = getResources();
-        mTodos = res.getStringArray(R.array.todos);
+        Log.d(TAG, " **** Just to say the PC is in onCreate!");
 
-        /* set the user interface layout for this Activity */
+        /* set the user interface layout for this Activity
+         the layout file is defined in the project res/layout/activity_todo.xml file */
         setContentView(R.layout.activity_todo);
 
+        /* check for saved state due to changes such as rotation or back button
+           and restore any saved state such as the todo_index */
+
+        /* TODO: Refactor to data layer */
+        Resources res = getResources();
+        mTodos = res.getStringArray(R.array.todo);
+
         /* initialize member TextView so we can manipulate it later */
-        TodoTextView = (TextView) findViewById(R.id.textViewTodo);
+        final TextView textViewTodo;
+        textViewTodo = (TextView) findViewById(R.id.textViewTodo);
 
-        /* display the first task from mTodo array in the TodoTextView */
-        TodoTextView.setText(mTodos[mTodoIndex]);
+        setTextViewComplete("");
 
-        /* setup navigation buttons */
-        /* next button to cycle through mTodos */
-        Button buttonNext;
-        buttonNext = (Button) findViewById(R.id.buttonNext);
+        /* display the first task from mTodo array in the textViewTodo */
+        textViewTodo.setText(mTodos[mTodoIndex]);
 
-        /* OnClick listener for the  Next button */
-        buttonNext.setOnClickListener(
-                new View.OnClickListener(){
+        Button buttonNext = (Button) findViewById(R.id.buttonNext);
+        buttonNext.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-
-                 //check index boundary for mTodos
-                if (mTodoIndex < mTodos.length - 1) {
-                    mTodoIndex += 1;
-                } else {
-                    mTodoIndex = 0;
-                }
-
-             //   mTodoIndex += 1;
-                TodoTextView.setText(mTodos[mTodoIndex]);
+                mTodoIndex = (mTodoIndex + 1) % mTodos.length;
+                textViewTodo.setText(mTodos[mTodoIndex]);
+                setTextViewComplete("");
             }
         });
 
-        /** TODO setup previous button object and listener
-        *   to cycle through mTodos */
-        Button buttonPrevios;
-        buttonPrevios = (Button) findViewById(R.id.buttonPrev);
 
-        buttonPrevios.setOnClickListener(
+        Button buttonTodoDetail = (Button) findViewById(R.id.buttonTodoDetail);
+        buttonTodoDetail.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
 
-                new View.OnClickListener(){
-                    @Override
-                    public void onClick(View v) {
-                        if (mTodoIndex!=0) //as cannot reverse back
-                        mTodoIndex=mTodoIndex-1;
-                        //   mTodoIndex += 1;
-                        TodoTextView.setText(mTodos[mTodoIndex]);
-                    }
-                });
+                /* Note, the child class being called has a static method determining the parameter
+                   to be passed to it in the intent object */
+                Intent intent = TodoDetailActivity.newIntent(TodoActivity.this, mTodoIndex);
 
-    }
+                /* second param requestCode identifies the call as there could be many "intents" */
+                startActivityForResult(intent, IS_SUCCESS);
+
+               /* The result will return through
+                   onActivityResult(requestCode, resultCode, Intent) method */
+            }
+        });
+
+
+
+    /** TODO setup previous button object and listener
+     *   to cycle through mTodos */
+    Button buttonPrevios;
+    buttonPrevios = (Button) findViewById(R.id.buttonPrev);
+
+        buttonPrevios.setOnClickListener(new View.OnClickListener(){
+
+        @Override
+        public void onClick(View v) {
+            if (mTodoIndex!=0) //as cannot reverse back
+                mTodoIndex=mTodoIndex-1;
+            //   mTodoIndex += 1;
+            textViewTodo.setText(mTodos[mTodoIndex]);
+        }
+    });
+
+}
+
 
     /*
-    This callback is called only when there is a saved instance that is previously saved by using
-    onSaveInstanceState(). We restore some state in onCreate(), while we can optionally restore
-    other state here, possibly usable after onStart() has completed.
-    The savedInstanceState Bundle is same as the one used in onCreate(). */
-
+        requestCode is the integer request code originally supplied to startActivityForResult
+        resultCode is the integer result code returned by the child activity through its setResult()
+        intent date attached with intent "extras"
+    */
     @Override
-    public void onRestoreInstanceState(Bundle savedInstanceState) {
-        mTodoIndex = savedInstanceState.getInt(TODO_INDEX);
-        TodoTextView.setText(mTodos[mTodoIndex]);
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+
+        if (requestCode == IS_SUCCESS ){
+            if (intent != null) {
+                // data in intent from child activity
+                boolean isTodoComplete = intent.getBooleanExtra(IS_TODO_COMPLETE, false);
+                updateTodoComplete(isTodoComplete);
+            } else {
+                Toast.makeText(this, R.string.back_button_pressed, Toast.LENGTH_SHORT).show();
+            }
+        } else {
+            Toast.makeText(this, R.string.request_code_mismatch,
+                    Toast.LENGTH_SHORT).show();
+        }
     }
 
 
-    /* invoked when the activity may be temporarily destroyed, save the instance state here */
-    @Override
-    public void onSaveInstanceState(Bundle savedInstanceState) {
-        savedInstanceState.putInt(TODO_INDEX, mTodoIndex);
-        // call superclass to save any view hierarchy
-        super.onSaveInstanceState(savedInstanceState);
+
+    private void updateTodoComplete(boolean is_todo_complete) {
+
+        final TextView textViewTodo;
+        textViewTodo = (TextView) findViewById(R.id.textViewTodo);
+
+        if (is_todo_complete) {
+            textViewTodo.setBackgroundColor(
+                    ContextCompat.getColor(this, R.color.backgroundSuccess));
+            textViewTodo.setTextColor(
+                    ContextCompat.getColor(this, R.color.colorSuccess));
+
+            setTextViewComplete("\u2713");
+        }
+
     }
+
+    private void setTextViewComplete( String message ){
+        final TextView textViewComplete;
+        textViewComplete = findViewById(R.id.textViewComplete);
+        textViewComplete.setText(message);
+    }
+
 }
